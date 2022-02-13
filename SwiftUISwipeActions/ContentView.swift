f9aa4b2e3da31b0fede3aea10b76495d3e5407d8
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-13 14:50:34 +0900

    first commit

--- SwiftUISwipeActions/ContentView.swift
+++ SwiftUISwipeActions/ContentView.swift
@@ -1,16 +1,28 @@
-//
-//  ContentView.swift
-//  SwiftUISwipeActions
-//
-//  Created by 大野颯太 on 2022/02/13.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let items = [
+        "朝起きたら勉強をする",
+        "洗濯をする",
+        "学校の課題をする",
+        "ご飯を作る",
+        "朝起きたら勉強をする",
+        "洗濯をする",
+        "学校の課題をする",
+        "ご飯を作る",
+        "朝起きたら勉強をする",
+        "洗濯をする",
+        "学校の課題をする",
+        "ご飯を作る"
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+               
+            }
+            .navigationTitle("To Do List")
+        }
     }
 }
 
--- SwiftUISwipeActions/SwiftUISwipeActionsApp.swift
+++ SwiftUISwipeActions/SwiftUISwipeActionsApp.swift
@@ -1,10 +1,3 @@
-//
-//  SwiftUISwipeActionsApp.swift
-//  SwiftUISwipeActions
-//
-//  Created by 大野颯太 on 2022/02/13.
-//
-
 import SwiftUI
 
 @main
