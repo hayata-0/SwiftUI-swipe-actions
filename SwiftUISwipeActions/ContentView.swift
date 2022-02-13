7b0ec2641927e6d60c05e5d81a6a48f564eca55a
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-13 14:42:18 +0900

    Initial Commit

---
+++ SwiftUISwipeActions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISwipeActions
+//
+//  Created by 大野颯太 on 2022/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISwipeActions/SwiftUISwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISwipeActionsApp.swift
+//  SwiftUISwipeActions
+//
+//  Created by 大野颯太 on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
